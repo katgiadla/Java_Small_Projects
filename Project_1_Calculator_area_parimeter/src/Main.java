1425c38b9a0a772bc45110802de8c3d406f5e800
katgiadla <katarzyna.giadla@gmail.com>
2019-03-03 23:23:48 +0100

    Create a function for traingle
    In code I added a new function to enter, compute and print data about traingle.

--- Project_1_Calculator_area_parimeter/src/Main.java
+++ Project_1_Calculator_area_parimeter/src/Main.java
@@ -1,78 +1,87 @@
+import jdk.jshell.spi.ExecutionControl;
+
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+    public void computeTriangle(){
+        boolean correctParametrs = false;
+        Scanner sc = new Scanner(System.in);
+        double side1 = 0, side2 = 0, side3 = 0;
+
+        do{
+            System.out.println("Please enter a first side");
+            try {
+                side1 = sc.nextDouble();
+            } catch (InputMismatchException e1){
+                System.out.println("Please enter a number");
+                correctParametrs = false;
+                continue;
+            }
+            correctParametrs = true;
+        } while(correctParametrs);
+
+        do{
+            System.out.println("Please enter a second side");
+            try {
+                side2 = sc.nextDouble();
+            } catch (InputMismatchException e1){
+                System.out.println("Please enter a number");
+                correctParametrs = false;
+                continue;
+            }
+            correctParametrs = true;
+        } while(correctParametrs);
+
+        do{
+            System.out.println("Please enter a third side");
+            try {
+                side3 = sc.nextDouble();
+            } catch (InputMismatchException e1){
+                System.out.println("Please enter a number");
+                correctParametrs = false;
+                continue;
+            }
+            correctParametrs = true;
+        } while(correctParametrs);
+
+        Triangle myTriangle = new Triangle(side1, side2, side3);
+        myTriangle.calculateArea();
+        myTriangle.calculatePerimetr();
+        myTriangle.print();
+        return;
+    }
+
     public static void main(String argv[]){
         Scanner scanner = new Scanner(System.in);
-        int userChoice = scanner.nextInt();
-        boolean loopVariable = true;
+        boolean loopVariable = false;
+        int userChoice = 0;
 
-        while (loopVariable == true) {
+        do {
             System.out.println("Which a figure do you want to compute?");
             System.out.println("1. Triangle");
             System.out.println("2. Square");
             System.out.println("3. Circle");
             try {
+
                 userChoice = scanner.nextInt();
-            } catch (IllegalArgumentException e) {
-                System.out.println("Please, entry a number");
-                loopVariable = true;
-            }
-        }
-        boolean correctParametrs = false;
-        switch (userChoice){
-            case 1: {
-                double side1 = 0, side2 = 0, side3 = 0;
-                while (!correctParametrs) {
-                    try{
-                        side1 = scanner.nextDouble();
-                        side2 = scanner.nextDouble();
-                        side3 = scanner.nextDouble();
-                    } catch (IllegalArgumentException e) {
-                        System.out.println("Please enter a number in double's format");
-                        correctParametrs = false;
-                    }
+                if (userChoice == 1 || userChoice == 2 || userChoice == 3) {
+                    break;
                 }
-                Triangle myTriangle = new Triangle(side1, side2, side3);
-                myTriangle.calculateArea();
-                myTriangle.calculatePerimetr();
-                myTriangle.print();
-                break;}
+                else{
+                    throw new IllegalArgumentException();}
+            } catch (InputMismatchException e1) {
+                System.out.println("Please enter a number");
+                scanner.nextLine();
+            } catch (IllegalArgumentException e2){
+                System.out.println("Please enter a correct number");
+                scanner.nextLine();
+            }
+        } while (!loopVariable);
 
-                case 2:
-                    double side = 0;
-                    while (!correctParametrs){
-                        try{
-                            side = scanner.nextDouble();
-                        } catch (IllegalArgumentException e){
-                            System.out.println("Please, enter a number in double's format");
-                            correctParametrs = false;
-                        }
-                    }
-                    Square mySquare = new Square(side);
-                    mySquare.calculateArea();
-                    mySquare.calculatePerimetr();
-                    mySquare.print();
-                    break;
-                case 3:
-                    double radius = 0;
-                    while (!correctParametrs) {
-                        try{
-                            radius = scanner.nextDouble();
-                        } catch (IllegalArgumentException e){
-                            System.out.println("Please, enter a number in double's format");
-                            correctParametrs = false;
-                        }
-                    }
-
-                    Circle myCircle = new Circle(radius);
-                    myCircle.calculateArea();
-                    myCircle.calculatePerimetr();
-                    myCircle.print();
-                    break;
-                 default:
-                     System.out.println("Please choose 1, 2 or 3");
-
-        }
 
     }
+
 }
+
