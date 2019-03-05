1d26454c89e755c20fa46573c5b7796f91437665
katgiadla <katarzyna.giadla@gmail.com>
2019-03-05 22:27:24 +0100

    Change a type of variable of sides
    I changed variable of sides from system "1 variable = 1 side" to array of sides

--- Project_1_Calculator_area_parimeter/src/Triangle.java
+++ Project_1_Calculator_area_parimeter/src/Triangle.java
@@ -1,26 +1,26 @@
 import java.util.Locale;
-import static java.lang.Math.*;
 
 public class Triangle extends Figure implements Print{
-    public double Side1, Side2, Side3;
     public double Area, Parimeter;
+    public double Sides[] = new double[3];
 
     public Triangle(double newSide1, double newSide2, double newSide3) {
-        this.Side1 = newSide1;
-        this.Side2 = newSide2;
-        this.Side3 = newSide3;
+        this.Sides[0] = newSide1;
+        this.Sides[1] = newSide2;
+        this.Sides[2] = newSide3;
     }
+
 
     @Override
     public double calculatePerimeter() {
-        this.Parimeter = this.Side1 + this.Side2 + this.Side3;
+        this.Parimeter = this.Sides[0] + this.Sides[1] + this.Sides[2];
         return this.Parimeter;
     }
 
     @Override
     public double calculateArea() {
-        double halfParimeter = (this.Side1 + this.Side2 + this.Side3)/2;
-        this.Area = halfParimeter * (halfParimeter-this.Side1) * (halfParimeter-this.Side2) * (halfParimeter-this.Side3);
+        double halfParimeter = (this.Sides[0] + this.Sides[1] + this.Sides[2])/2;
+        this.Area = halfParimeter * (halfParimeter-this.Sides[0]) * (halfParimeter-this.Sides[1]) * (halfParimeter-this.Sides[2]);
         this.Area = Math.sqrt(this.Area);
         return this.Area;
     }
@@ -28,7 +28,7 @@ public class Triangle extends Figure implements Print{
     @Override
     public void print() {
         System.out.println("It's triangle");
-        System.out.println(String.format(Locale.US, "Side 1: %.2f, Side 2: %.2f, Side3: %.2f", this.Side1, this.Side2, this.Side3 ));
+        System.out.println(String.format(Locale.US, "Side 1: %.2f, Side 2: %.2f, Side3: %.2f", this.Sides[0], this.Sides[1], this.Sides[2]));
         System.out.println(String.format(Locale.US, "Parimeter: %.2f", this.Parimeter));
         System.out.println(String.format(Locale.US, "Area: %.2f", this.Area));
         return;
