4ccae956577f903ea54de230f5e17ded3d31e96b
katgiadla <katarzyna.giadla@gmail.com>
2019-03-07 15:43:29 +0100

    Add a contructor

---
+++ Project_2_Simple_storage_simulator/src/Item.java
@@ -0,0 +1,13 @@
+public class Item {
+    String name;
+    ItemCondition condition;
+    Double weight;
+    Integer amount;
+
+    public Item(String newName, ItemCondition newCondition, Double newWeigtht, Integer newAmount){
+        this.name = newName;
+        this.condition = newCondition;
+        this.weight = newWeigtht;
+        this.amount = newAmount;
+    }
+}
