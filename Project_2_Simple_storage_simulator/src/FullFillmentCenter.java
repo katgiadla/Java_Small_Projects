c353aff90a0f9259e788e378c20f572de5ee6846
katgiadla <katarzyna.giadla@gmail.com>
2019-03-11 23:20:49 +0100

    Add new function

--- Project_2_Simple_storage_simulator/src/FullFillmentCenter.java
+++ Project_2_Simple_storage_simulator/src/FullFillmentCenter.java
@@ -1,10 +1,25 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
-public class FullFillmentCenter {
+public class FullFillmentCenter implements Print {
     String nameOfFillment;
     List<Item> listOfProducts = new ArrayList<>(); //interface
-    Integer maxCapacitance = listOfProducts.size();
+    Integer maxCapacitance = 10;
 
+    public void addProduct(Item newItem){ }
+
+    public void getProduct(Item itemToGet){}
+
+    public void removeProduct(Item itemToRemove) {}
+
+    public void search(String nameofProduct){}
+
+    public void searchPartial(String searchName){}
+
+    public void countByCondition(ItemCondition conditionToDisplay){}
+
+    @Override
+    public void print() {}
 
 }
