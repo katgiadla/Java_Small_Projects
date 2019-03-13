c006d6dcde807030ba943dae71ab579faf2e6de2
katgiadla <katarzyna.giadla@gmail.com>
2019-03-13 18:51:34 +0100

    Update a name of file and class
    Currently name have a language mistake

---
+++ Project_2_Simple_storage_simulator/src/FulfilmentCenter.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.stream.IntStream;
+
+public class Fulfilment {
+    String nameOfFillmentCenter;
+    List<Item> listOfProducts = new ArrayList<>(); //interface
+    Integer maxCapacitance = 15;
+
+    public void addProduct(Item newItem){
+        Integer currentAmountOfProducts = 0;
+        for (Item iterator: listOfProducts){ currentAmountOfProducts += iterator.amount;}
+        if (currentAmountOfProducts + newItem.amount > maxCapacitance){
+            throw new IllegalArgumentException();
+        }
+        for(Item iterator: listOfProducts){
+            if (iterator.name.compareToIgnoreCase(newItem.name) == 0){
+                    System.out.println("Old product has added correctly!");
+                    iterator.amount += newItem.amount;
+                    return;
+            }
+        }
+        System.out.println("New product has added correctly!");
+        listOfProducts.add(newItem);
+    }
+}
