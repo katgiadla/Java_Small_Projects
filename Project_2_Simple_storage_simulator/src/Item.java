a5537a45c7cd26e429e4e1a2fa5b4a32cc286d3c
katgiadla <katarzyna.giadla@gmail.com>
2019-03-13 18:53:32 +0100

    Add definition of function to add a product

--- Project_2_Simple_storage_simulator/src/Item.java
+++ Project_2_Simple_storage_simulator/src/Item.java
@@ -14,9 +14,9 @@ public class Item implements Print, Comparable<Item>{
     @Override
     public void print() {
         System.out.println(String.format("Product: %s", this.name));
-        System.out.println(String.format("Condition: " + this.condition));
+        System.out.println("Condition: " + this.condition);
         System.out.println(String.format("Weight: %.2f", this.weight));
-        System.out.println(String.format("Amount: %i", this.amount));
+        System.out.println(String.format("Amount: %d", this.amount));
     }
 
     @Override
