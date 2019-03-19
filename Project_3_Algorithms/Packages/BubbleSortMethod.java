598d610242e75fd45a0f842e4a17461e7c729d78
katgiadla <katarzyna.giadla@gmail.com>
2019-03-19 23:28:38 +0100

    Create class for bubble sort and my own exception

---
+++ Project_3_Algorithms/Packages/BubbleSortMethod.java
@@ -0,0 +1,15 @@
+public class BubbleSortMethod extends SortWay{
+
+    @Override
+    public void sortOnMyWay(Integer[] arr, Integer size) throws ZeroElementsException{
+        System.out.print("Array before sort: ");
+        printArray(arr);
+        if (arr.length == 0) {throw new ZeroElementsException();}
+        if (size == 1) {return;}
+        for (int i = 0; i < arr.length; i++){
+            if(arr[i] > arr[i+1]){swap(arr[i], arr[i+1]);} }
+        sortOnMyWay(arr, size-1);
+        printArray(arr);
+        checkTimeOfSort();
+    }
+}
