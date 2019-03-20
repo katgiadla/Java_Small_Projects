836aa1967a02adf445a307ad45bcd2ca48859b12
katgiadla <katarzyna.giadla@gmail.com>
2019-03-20 22:41:50 +0100

    Create Main class

--- Project_3_Algorithms/src/Main.java
+++ Project_3_Algorithms/src/Main.java
@@ -1,5 +1,13 @@
+import packageSort.*;
+
 public class Main {
     public static void main(String[] argv){
-
+        Integer[] arr = {2,5, 5,6,8,3};
+        BubbleSortMethod newBubbleSort = new BubbleSortMethod();
+        try {
+            newBubbleSort.sortOnMyWay(arr, arr.length);
+        } catch (ZeroElementsException e){
+            System.err.println("Zero element in array!");
+        }
     }
 }
