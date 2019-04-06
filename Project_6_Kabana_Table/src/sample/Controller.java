98f93433c43e6789fa4117497e74d46bb14e8b3a
katgiadla <katarzyna.giadla@gmail.com>
2019-04-06 19:01:33 +0200

    Add control to the panel

--- Project_6_Kabana_Table/src/sample/Controller.java
+++ Project_6_Kabana_Table/src/sample/Controller.java
@@ -1,4 +1,10 @@
 package sample;
 
+import javafx.scene.control.ScrollPane;
+
 public class Controller {
+
+    final ScrollPane TODO = new ScrollPane();
+    final ScrollPane processingTask = new ScrollPane();
+    final ScrollPane doneTask = new ScrollPane();
 }
