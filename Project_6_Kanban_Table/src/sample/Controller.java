a62e5624462c5a92d7da1d971a4ff5e71adc5700
katgiadla <katarzyna.giadla@gmail.com>
2019-04-09 18:54:41 +0200

    Add a button and check their work

--- Project_6_Kanban_Table/src/sample/Controller.java
+++ Project_6_Kanban_Table/src/sample/Controller.java
@@ -6,38 +6,50 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
 
 public class Controller {
 
     @FXML
-    private ResourceBundle resources;
+    private ListView<Task> listTODO;
 
     @FXML
-    private URL location;
+    private ListView<Task> listDoing;
 
     @FXML
-    private ScrollPane listTODO;
+    private Label labelDoing;
+
+    @FXML
+    private GridPane mainWindow;
 
     @FXML
     private Button newTaskButton;
 
     @FXML
-    private ScrollPane listProcessing;
+    private Label labelDone;
 
     @FXML
-    private ScrollPane listDone;
+    private Label labelTODO;
+
+    @FXML
+    private ListView<Task> listDone;
 
     @FXML
     void addNewTask(ActionEvent event) {
-        System.out.println("I don't undestand");
+
     }
 
     @FXML
     void initialize() {
         assert listTODO != null : "fx:id=\"listTODO\" was not injected: check your FXML file 'sample.fxml'.";
+        assert listDoing != null : "fx:id=\"listDoing\" was not injected: check your FXML file 'sample.fxml'.";
+        assert labelDoing != null : "fx:id=\"labelDoing\" was not injected: check your FXML file 'sample.fxml'.";
+        assert mainWindow != null : "fx:id=\"mainWindow\" was not injected: check your FXML file 'sample.fxml'.";
         assert newTaskButton != null : "fx:id=\"newTaskButton\" was not injected: check your FXML file 'sample.fxml'.";
-        assert listProcessing != null : "fx:id=\"listProcessing\" was not injected: check your FXML file 'sample.fxml'.";
+        assert labelDone != null : "fx:id=\"labelDone\" was not injected: check your FXML file 'sample.fxml'.";
+        assert labelTODO != null : "fx:id=\"labelTODO\" was not injected: check your FXML file 'sample.fxml'.";
         assert listDone != null : "fx:id=\"listDone\" was not injected: check your FXML file 'sample.fxml'.";
 
     }
--- Project_6_Kanban_Table/src/sample/Task.java
+++ Project_6_Kanban_Table/src/sample/Task.java
@@ -2,9 +2,56 @@ package sample;
 
 import java.time.LocalDate;
 
+import static sample.Priority.*;
+
 public class Task {
-    String nameOfTask;
-    LocalDate expiryDate;
-    String priority;
-    String descriptionOfTask;
+
+    private String title;
+    private Priority priority;
+    private LocalDate localDate;
+    private String description;
+
+
+    public String getTitle() {
+        return title;
+    }
+    public Priority getPriority() {
+        return priority;
+    }
+    public LocalDate getLocalDate(){
+        return localDate;
+    }
+    public String getDescription(){
+        return description;
+    }
+
+
+    public void setTitle(String title){
+        this.title=title;
+    }
+    public void setPriority(Priority priority ){
+        this.priority =priority ;
+    }
+    public void setLocalDate (LocalDate localDate ){
+        this.localDate =localDate ;
+    }
+
+
+    public Task() {
+        this.title = "JavaFx";
+        this.description = " implement kanban method ";
+        this.priority = tommorow;
+        this.localDate = LocalDate.of(2019, 4, 8);
+    }
+
+    public Task(String title, Priority priority, LocalDate localDate, String description) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.localDate = localDate;
+    }
+
+
+
+
 }
